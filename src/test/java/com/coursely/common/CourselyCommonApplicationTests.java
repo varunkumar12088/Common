b0bbd9842f6ea41dc9c050e522723e0d0a3647f6
Varun Kumar <varun.kumar12aug@gmail.com>
2025-07-05 12:07:29 +0530

    First time adding

---
+++ src/test/java/com/coursely/common/CourselyCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursely.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourselyCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
