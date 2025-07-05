b6e83dddea53c6b813480b6f9348c8946d4f37b1
Varun Kumar <varun.kumar12aug@gmail.com>
2025-07-05 12:48:05 +0530

    rename package

--- src/main/java/com/metaskillsacademy/common/CourselyCommonApplication.java
+++ src/main/java/com/metaskillsacademy/common/CourselyCommonApplication.java
@@ -1,4 +1,4 @@
-package com.coursely.common;
+package com.metaskillsacademy.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/metaskillsacademy/common/config/MongoConfig.java
@@ -0,0 +1,46 @@
+package com.metaskillsacademy.common.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MongoConfig.class);
+
+    @Value("${database.name}")
+    private String databaseName;
+
+    @Override
+    protected String getDatabaseName() {
+        return this.databaseName;
+    }
+
+    @Override
+    @Bean
+    public MongoClient mongoClient() {
+        String username = System.getenv("MONGO_USERNAME");
+        String password = System.getenv("MONGO_PASSWORD");
+        String host = System.getenv("MONGO_HOST");
+
+        if (StringUtils.isAnyBlank(username, password, host)) {
+            LOGGER.error("MongoDB connection details are not set in environment variables.");
+            throw new IllegalStateException("MongoDB connection details are not set.");
+        }
+
+        String connectionString = String.format(
+                "mongodb+srv://%s:%s@%s/%s?retryWrites=true&w=majority",
+                username, password, host, this.databaseName
+        );
+        LOGGER.info("Connecting to MongoDB: {}", connectionString);
+        return MongoClients.create(connectionString);
+    }
+}
